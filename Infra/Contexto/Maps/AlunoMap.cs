35b28866b0000cef0860ead547c0838feb8920a9
Douglasnorbbitt <66019038+Douglasnorbbitt@users.noreply.github.com>
2021-04-07 18:46:24 -0400

    Update AlunoMap.cs
    Mapeamento da classe aluno feita

--- Infra/Contexto/Maps/AlunoMap.cs
+++ Infra/Contexto/Maps/AlunoMap.cs
@@ -1,11 +1,21 @@
-﻿using System;
+using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Infra.Contexto.Maps
 {
-    public class AlunoMap
+    public class AlunoMap : IEntityTypeConfiguration<Aluno>
     {
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder.ToTable("Alunos");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(100).HasColumnType("varchar(100)");
+            builder.Property(x => x.Matricula).HasMaxLength(20).HasColumnType("varchar(20)"); ;
+        }
 
     }
 }
